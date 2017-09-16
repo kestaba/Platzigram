e0eec16753288b71fcbe10b6e1a72c6a26abb47a
Ana <developer.try.catch@gmail.com>
2017-09-16 00:39:41 -0400

    Maquetación y funcionalidad para Detalle de imagen.

--- app/src/main/java/com/platzi/platzigram/adapter/PictureAdapterRecyclerView.java
+++ app/src/main/java/com/platzi/platzigram/adapter/PictureAdapterRecyclerView.java
@@ -1,6 +1,7 @@
 package com.platzi.platzigram.adapter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 
 import com.platzi.platzigram.R;
 import com.platzi.platzigram.model.Picture;
+import com.platzi.platzigram.view.PictureDetailActivity;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -43,6 +45,14 @@ public class PictureAdapterRecyclerView extends RecyclerView.Adapter<PictureAdap
         holder.timeCard.setText(picture.getTime());
         holder.likeNumberCard.setText(picture.getLike_number());
         Picasso.with(activity).load(picture.getPicture()).into(holder.pictureCard);
+
+        holder.pictureCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity, PictureDetailActivity.class);
+                activity.startActivity(intent);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/platzi/platzigram/view/PictureDetailActivity.java
@@ -0,0 +1,30 @@
+package com.platzi.platzigram.view;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.platzi.platzigram.R;
+
+public class PictureDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_detail);
+
+        showToolbar("", true);
+    }
+
+    public void showToolbar(String title, boolean upButton){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbar);
+
+    }
+}
