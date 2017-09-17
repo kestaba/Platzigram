45dd8c86acab9410d3e6ff9ab1136150853ad788
Karina Estaba <karina.estaba@gmail.com>
2017-09-16 20:17:08 -0400

    Maquetación de fragment_search

--- app/src/main/java/com/platzi/platzigram/view/PictureDetailActivity.java
+++ app/src/main/java/com/platzi/platzigram/view/PictureDetailActivity.java
@@ -1,9 +1,12 @@
 package com.platzi.platzigram.view;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.transition.Fade;
 import android.view.View;
 
 import com.platzi.platzigram.R;
@@ -16,6 +19,11 @@ public class PictureDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_picture_detail);
 
         showToolbar("", true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+            getWindow().setEnterTransition(new Fade());
+        }
     }
 
     public void showToolbar(String title, boolean upButton){
